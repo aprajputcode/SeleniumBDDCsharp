3b30f8d60a55d807fbf1b437a32dd66116dbe862
Akshay Pardeshi <akshay.pardeshi.7373@gmail.com>
2025-03-25 18:30:34 +0530

    Added asserts.cs file

---
+++ Utility/Asserts.cs
@@ -0,0 +1,39 @@
+﻿
+using NUnit.Framework;
+
+namespace Demo.Source.Utility
+{
+    public class Asserts
+    {
+        private Asserts() { }
+
+        /**
+        * this method is used to compare expected and actual text
+        */
+        public static void AssertEqual(String actual, String expected)
+        {
+            try
+            {
+                Assert.IsTrue(expected.Equals(actual.Trim()));
+            }
+            catch
+            {
+                throw new Exception($"Expected:{expected} \nActual:{actual}");
+            }
+        }
+
+        /**
+        * this method is used to compare expected value and actual contains text  
+        */
+        public static void AssertsContainsTexts(string actual, String containsText)
+        {
+            try
+            {
+                Assert.IsTrue(actual.Contains(containsText));
+            }catch
+            {
+                throw new Exception($"Actual:{actual} \nExpected Contains Text:{containsText}");
+            }
+        }
+    }
+}
